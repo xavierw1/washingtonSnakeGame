c8ea4687da1b9b0733a1986bbce1ec9d7ae1fe02
Washington <xavierw1@wloo.org>
2019-10-30 11:04:33 -0500

    End of Class 10/30/19

--- script.jss.js
+++ script.jss.js
@@ -28,3 +28,51 @@ function setup() {
     Textsize(18);
 
 }
+function draw() {
+    background(255);
+    displayScore();
+    if(gamePlaying){
+        addApple();
+        drawSnake();
+    } else {
+        textAlign(CENTER);
+        text('press UP, RIGHT, or DOWN arrows to begin',width / 2,height / 2);
+        fill(0, 255, 0);
+        for(var i  = 0; i < snakeArray.lenth; i++) {
+            rect(snakeArray[i].xPos, snakeArray[i].yPos, 19, 19);
+       }
+       addapple();
+    }    
+}
+
+function loadBestScore() {
+    var bestscore = getItem('bestScore');
+    if(bestScore) {
+        return bestScore;
+    }
+    return 0; 
+}
+
+function displayScore() {
+    line(0, 40, width, 40,);
+    line(0, 0, 0, 480);
+    line(0, 480, 400, 480);
+    line(400, 480, 400, 0);
+    line(0, 0, 400, 0);
+    fill(0, 102, 153);
+    textAlign(LEFT);
+    text('Score', 30, 27);
+    text( score, 100, 27);
+
+    text('Best Score', 230, 27);
+    text(bestScore, 340, 27);
+
+}
+
+function makeSnakePiece(x,y) {
+    snakePiece = {
+        xPos: x,
+        ypos: Y
+    }
+    append(snakeArray, snakePiece);
+}
